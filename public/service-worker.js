00ad104c8f50d249ba60c524628af031f121bad5
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-14 17:25:54 -0400

    cache update

--- public/service-worker.js
+++ public/service-worker.js
@@ -46,3 +46,29 @@ const FILES_TO_CACHE = [
   });
 
   // Fetch
+  self.addEventListener("fetch", function(event) {
+      // API will return JSON
+      if (event.request.url.includes("/api")) {
+          event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    // Clone and store in cache with successful response
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                }).catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+          );
+      } else {
+          event.respondWith(
+              caches.open(CACHE_NAME).then(cache => {
+                  return cache.match(event.request).then(response => {
+                      return response || fetch(event.request);
+                  });
+              })
+          );
+      }
+  });
