5aaf525a6e127bda9dcf1dbed1230cbbf32edcf4
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-16 00:30:16 -0400

    final mongoose connection with atlas

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
